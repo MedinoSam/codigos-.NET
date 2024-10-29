0dfd6a19d4a57d19a26497082a5ad5f36264353a
MedinoSam <samuelejosuetutorial@gmail.com>
2024-10-29 08:37:11 -0300

    exercicio piramide

---
+++ PrimeiraLista/Piramide.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiraLista
+{
+    public class Piramide
+    {
+        public int N { get; set; }
+
+        public Piramide(int n)
+        {
+            this.N = n;
+        }
+
+        public void Desenha(int n)
+        {
+            if (n < 1)
+            {
+                throw new NotFiniteNumberException();
+            }
+            for (int i = 1; i <= N; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                for (int j = i-1; j > 0; j--)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+}
--- PrimeiraLista/Program.cs
+++ PrimeiraLista/Program.cs
@@ -1,2 +1,11 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using PrimeiraLista;
+
+Piramide piramide = new Piramide(4);
+try
+{
+    piramide.Desenha(4);
+}
+catch (NotFiniteNumberException)
+{
+    Console.WriteLine("O numero precisa ser maior ou igual a 1");
+}
