f3321b6461c151661734fb2963bbd97dc1f87bfa
MedinoSam <samuelejosuetutorial@gmail.com>
2024-10-30 00:33:00 -0300

    motor e carro

---
+++ PrimeiraLista/Motor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiraLista
+{
+    public class Motor
+    {
+        public double Cilindrada { get; private set; }
+        public Carro Car { get; private set; }
+
+        public Motor(double cilindrada) 
+        {
+            Cilindrada = cilindrada;
+        }
+        public void InstalarEmCarro(Carro carro)
+        {
+            if (carro != null)
+                throw new InvalidOperationException("O motor esta  em outro carro.");
+
+            Car = carro;
+        }
+
+        public void RemoverDeCarro()
+        {
+            Car = null;
+        }
+    }
+}
