488b8414751deb074b544509e95ff77514d37a5e
MedinoSam <samuelejosuetutorial@gmail.com>
2024-10-30 00:06:38 -0300

    pessoa e certidao de nascimento

---
+++ PrimeiraLista/CertidaoNascimento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiraLista
+{
+    internal class CertidaoNascimento
+    {
+        public DateTime DataEmissao { get; private set; }
+        public Pessoa Pessoa { get; private set; }
+
+        public CertidaoNascimento(DateTime dataEmissao, Pessoa pessoa)
+        {
+            DataEmissao = dataEmissao;
+            Pessoa = pessoa;
+        }
+
+    }
+}
