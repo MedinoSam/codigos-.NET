47b6f68de31d17845c2a928a7a11b7043364a5e4
MedinoSam <samuelejosuetutorial@gmail.com>
2024-10-29 23:06:10 -0300

    lista intervalos

--- PrimeiraLista/ListaIntervalo.cs
+++ PrimeiraLista/ListaIntervalo.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,39 @@ namespace PrimeiraLista
 {
     internal class ListaIntervalo
     {
-        public List<Intervalo> intervalos;
+        public List<Intervalo> IntervalosList;
+
+        public ListaIntervalo(List<Intervalo> intervalos)
+        {
+            IntervalosList = intervalos;
+        }
+
+        public ImmutableList<Intervalo> Intervalos
+        {
+            get
+            {
+                return IntervalosList.OrderBy(i => i.DataHoraInicial).ToImmutableList();
+            }
+        }
+
+
+        public bool ContemIntersecao(Intervalo a)
+        {
+            foreach (Intervalo item in IntervalosList)
+            {
+                if (item.TemIntersecao(a))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void Add(Intervalo a)
+        {
+            if (!ContemIntersecao(a))
+                {
+                IntervalosList.Add(a);
+                }
+        }
     }
 }
